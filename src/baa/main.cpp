c17b4f8a7a6e9e1d323bca220219fd09e5fa52ea
Eugene Khashin <adastreamer@gmail.com>
2023-07-11 02:31:23 +0400

    args parsing

--- src/baa/main.cpp
+++ src/baa/main.cpp
@@ -4,15 +4,17 @@
 #include <security/pam_ext.h>
 #include <security/pam_modules.h>
 
+#include <curl/curl.h>
+
 const char *BACKEND_URL_KEY = "backend_url=";
 const char *SSL_VERIFY_IGNORE_KEY = "ssl_verify_ignore=";
 
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     int retval;
-    const char *user=NULL;
-    const char *password=NULL;
-    const char *backend_url=NULL;
-    const char *ssl_verify_ignore=NULL;
+    const char *user = NULL;
+    const char *password = NULL;
+    const char *backend_url = NULL;
+    bool ssl_verify_ignore = false;
 
     retval = pam_get_user(pamh, &user, NULL);
     if (retval != PAM_SUCCESS) return retval;
@@ -26,18 +28,20 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
     for (int i = 0; i < argc; i++) {
         int arglen = strlen(argv[i]);
 
-        if ((arglen <= backend_url_key_len) &&
-            (arglen <= ssl_verify_ignore_key_len)) continue;
+        if (arglen > backend_url_key_len) {
+            for (j = 0; j < backend_url_key_len; j++)
+                if (argv[i][j] != BACKEND_URL_KEY[j]) break;
+            if (j == backend_url_key_len)
+                backend_url = &argv[i][backend_url_key_len];
+        }
 
-        for (j = 0; j < backend_url_key_len; j++)
-            if (argv[i][j] != BACKEND_URL_KEY[j]) break;
-        if (j == backend_url_key_len)
-            backend_url = &argv[i][backend_url_key_len];
-
-        for (j = 0; j < ssl_verify_ignore_key_len; j++)
-            if (argv[i][j] != SSL_VERIFY_IGNORE_KEY[j]) break;
-        if (j == ssl_verify_ignore_key_len)
-            ssl_verify_ignore = &argv[i][ssl_verify_ignore_key_len];
+        if (arglen > ssl_verify_ignore_key_len) {
+            for (j = 0; j < ssl_verify_ignore_key_len; j++)
+                if (argv[i][j] != SSL_VERIFY_IGNORE_KEY[j]) break;
+            if (j == ssl_verify_ignore_key_len)
+                if (argv[i][ssl_verify_ignore_key_len] == '1')
+                    ssl_verify_ignore = true;
+        }
     }
 
     if (!backend_url)
